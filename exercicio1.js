e34237b88dcd2c468021f46d816552ecfb7e6276
Henrique Hideki Sampaio <Henrique Hideki Sampaio>
2017-04-11 11:52:19 -0300

    entrega atividade

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,13 @@
 // Faça o exercício dos parágrafos aqui
 // Este arquivo ainda não está incluído no arquivo HTML
+let x = document.getElementsByClassName('botao-expandir-retrair');
+for (let i = x.length - 1; i >= 0; i--) {
+	x[i].addEventListener('click', function(e){
+		let p = this.parentNode;
+		if(p.classList.contains('expandido')){
+			p.classList.remove('expandido');
+		}else {
+			p.classList.add('expandido');
+		}
+	});
+}
--- exercicio2.js
+++ exercicio2.js
@@ -1,5 +1,6 @@
-// Faça o exercício da galeria de imagens aqui
+
 // Este arquivo ainda não está incluído no arquivo HTML
+
 
 const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
   todasAsImagens = [
@@ -9,3 +10,39 @@ const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
     'philae-67-picture.jpg',
     'philae-collecting.jpg'
   ];
+
+let slider = function(Index){
+
+	let slide = document.getElementById('slide')
+	let srcImage  = servidorDasImagens + todasAsImagens[Index] ;
+	slide.src = srcImage;
+};
+
+
+let index = 0;
+slider(index);
+
+let prox = document.getElementById('proximo');
+	prox.addEventListener('click', function(e){
+		index = index + 1;
+		if( index == 5){
+			index =0;
+		}
+		slider(index);
+	});
+
+
+let ant = document.getElementById('anterior');
+	ant.addEventListener('click', function(e){
+		index = index - 1;
+		if( index == -1){
+			index =4;
+		}
+		slider(index);
+	});
+
+
+
+
+
+
